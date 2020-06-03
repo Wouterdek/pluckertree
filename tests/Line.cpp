239f4a97a080b43c2a7f2e4bc54ff872922ceb14
Wouter <wouterdekrsmckr@gmail.com>
2020-06-03 23:30:22 +0200

    Fix line tests

--- tests/Line.cpp
+++ tests/Line.cpp
@@ -12,7 +12,7 @@ TEST(Line, TestFromPointAndDirection1)
     auto epsilon = 1e-6;
     auto l = Line::FromPointAndDirection(Vector3f(0, 0, 1), Vector3f(1, 0, 0));
     EXPECT_VEC3_NEAR(l.d, Vector3f(1, 0, 0), epsilon);
-    EXPECT_VEC3_NEAR(l.m, Vector3f(M_PI/2, M_PI/2, 1), epsilon);
+    EXPECT_VEC3_NEAR(l.m, Vector3f(0, 1, 0), epsilon);
 }
 
 TEST(Line, TestFromPointAndDirection2)
@@ -20,8 +20,7 @@ TEST(Line, TestFromPointAndDirection2)
     auto epsilon = 1e-6;
     auto l = Line::FromPointAndDirection(Vector3f(2, 0, 0), Vector3f(0, 1, 0));
     EXPECT_VEC3_NEAR(l.d, Vector3f(0, 1, 0), epsilon);
-    EXPECT_NEAR(l.m[1], 0, epsilon);
-    EXPECT_NEAR(l.m[2], 2, epsilon);
+    EXPECT_VEC3_NEAR(l.m, Vector3f(0, 0, 2), epsilon);
 }
 
 TEST(Line, TestFromPointAndDirection3)
@@ -29,7 +28,7 @@ TEST(Line, TestFromPointAndDirection3)
     auto epsilon = 1e-6;
     auto l = Line::FromPointAndDirection(Vector3f(1, 0, 1), Vector3f(1, 0, 0));
     EXPECT_VEC3_NEAR(l.d, Vector3f(1, 0, 0), epsilon);
-    EXPECT_VEC3_NEAR(l.m, Vector3f(M_PI/2, M_PI/2, 1), epsilon);
+    EXPECT_VEC3_NEAR(l.m, Vector3f(0, 1, 0), epsilon);
 }
 
 TEST(Line, TestFromPointAndDirection4)
@@ -37,7 +36,7 @@ TEST(Line, TestFromPointAndDirection4)
     auto epsilon = 1e-6;
     auto l = Line::FromPointAndDirection(Vector3f(-1, 0, 0), Vector3f(0, 0, 1));
     EXPECT_VEC3_NEAR(l.d, Vector3f(0, 0, 1), epsilon);
-    EXPECT_VEC3_NEAR(l.m, Vector3f(M_PI/2, M_PI/2, 1), epsilon);
+    EXPECT_VEC3_NEAR(l.m, Vector3f(0, 1, 0), epsilon);
 }
 
 TEST(Line, TestFromPointAndDirection5)
@@ -46,7 +45,7 @@ TEST(Line, TestFromPointAndDirection5)
     Vector3f d = Vector3f(-1/std::sqrt(2.0f), -1/std::sqrt(2.0f), 1).normalized();
     auto l = Line::FromPointAndDirection(Vector3f(1, -1, 0).normalized(), d);
     EXPECT_VEC3_NEAR(l.d, d, epsilon);
-    EXPECT_VEC3_NEAR(l.m, Vector3f(-3*M_PI/4, 3*M_PI/4, 1), epsilon);
+    EXPECT_VEC3_NEAR(l.m, Vector3f(-0.5, -0.5, -std::sqrt(2)/2), epsilon);
 }
 
 TEST(Line, TestFromTwoPoints1)
@@ -54,7 +53,7 @@ TEST(Line, TestFromTwoPoints1)
     auto epsilon = 1e-6;
     auto l = Line::FromTwoPoints(Vector3f(0, 0, 1), Vector3f(1, 0, 1));
     EXPECT_VEC3_NEAR(l.d, Vector3f(1, 0, 0), epsilon);
-    EXPECT_VEC3_NEAR(l.m, Vector3f(M_PI/2, M_PI/2, 1), epsilon);
+    EXPECT_VEC3_NEAR(l.m, Vector3f(0, 1, 0), epsilon);
 }
 
 TEST(Line, TestFromTwoPoints2)
@@ -62,8 +61,7 @@ TEST(Line, TestFromTwoPoints2)
     auto epsilon = 1e-6;
     auto l = Line::FromTwoPoints(Vector3f(2, 0, 0), Vector3f(2, 2, 0));
     EXPECT_VEC3_NEAR(l.d, Vector3f(0, 1, 0), epsilon);
-    EXPECT_NEAR(l.m[1], 0, epsilon);
-    EXPECT_NEAR(l.m[2], 2, epsilon);
+    EXPECT_VEC3_NEAR(l.m, Vector3f(0, 0, 2), epsilon);
 }
 
 TEST(Line, TestFromTwoPoints3)
@@ -71,5 +69,5 @@ TEST(Line, TestFromTwoPoints3)
     auto epsilon = 1e-6;
     auto l = Line::FromTwoPoints(Vector3f(1, 0, 1), Vector3f(2, 0, 1));
     EXPECT_VEC3_NEAR(l.d, Vector3f(1, 0, 0), epsilon);
-    EXPECT_VEC3_NEAR(l.m, Vector3f(M_PI/2, M_PI/2, 1), epsilon);
+    EXPECT_VEC3_NEAR(l.m, Vector3f(0, 1, 0), epsilon);
 }
